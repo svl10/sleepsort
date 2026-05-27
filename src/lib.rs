4de793ec1869607e1356a4f00dfe22334be03aaa
svl10 <seanvl123@gmail.com>
2026-05-27 21:59:56 +0800

    Setup args

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::{thread, time::Duration};
 
-fn sleepsort(list: &Vec<i32>){
+pub fn sleepsort(list: &Vec<i32>){
 
     for &i in list{
         thread::spawn(move || {
-            thread::sleep(Duration::from_secs(i as u64));
+            thread::sleep(Duration::from_millis(i as u64));
             print!("{} ", i);
     });
     }
+    let highest = list.iter().max().unwrap();
+    thread::sleep(Duration::from_millis(*highest as u64));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use sleepsort;
-
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let thevec: Vec<String> = env::args().collect();
+
+    let mut newvec: Vec<i32> = vec![];
+    for i in &thevec[1..]{
+        let value: i32 = i.parse().expect("shet");
+        newvec.push(value);
+    }
+    sleepsort::sleepsort(&newvec);
+    // println!("{:?}", newvec);
 }
